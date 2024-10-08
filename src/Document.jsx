f33f79e2745ef42a5d77994d8a3cda8c8a51468b
malkiAbdoo <abdo.malkiep@gmail.com>
2024-10-08 18:28:23 +0100

    fix: add font registration

--- src/Document.jsx
+++ src/Document.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-
-/**
- * Create styles
- * @see https://react-pdf.org/styling#stylesheet-api
- */
-const styles = StyleSheet.create({
-  section: {
-    padding: 10,
-    width: '100%',
-    height: '100%',
-    border: '1px solid #000',
-  },
-});
+import { Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
 
 /**
  * Create Document Component
@@ -21,9 +8,9 @@ const styles = StyleSheet.create({
 export const DocumentPost = () => {
   return (
     <Document>
-      <Page size={[1080, 1350]}>
+      <Page size={[1080, 1350]} style={styles.body}>
         {new Array(10).fill(0).map((_, i) => (
-          <View key={i} style={styles.section}>
+          <View key={i} style={styles.page}>
             <Text>Section #{i + 1}</Text>
           </View>
         ))}
@@ -31,3 +18,39 @@ export const DocumentPost = () => {
     </Document>
   );
 };
+
+/**
+ * Register the font
+ * @see https://react-pdf.org/fonts#fonts
+ */
+Font.register({
+  family: 'Geist',
+  fonts: [
+    {
+      src: 'fonts/Geist-Bold.otf',
+      fontWeight: 'bold',
+    },
+    {
+      src: 'fonts/Geist-Medium.otf',
+      fontStyle: 'normal',
+      fontWieght: 'medium',
+    },
+  ],
+});
+
+/**
+ * Create styles
+ * @see https://react-pdf.org/styling#stylesheet-api
+ */
+const styles = StyleSheet.create({
+  body: {
+    fontFamily: 'Geist',
+    fontWeight: 'medium',
+    fontSize: 36,
+  },
+  page: {
+    padding: 10,
+    width: '100%',
+    height: '100%',
+  },
+});
